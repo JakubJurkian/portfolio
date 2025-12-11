c0056042c010a151ea60042fd760146104dca3ee
JakubJurkian <kuba.jur03@gmail.com>
2025-12-11 14:10:31 +0100

    feat: add styles for main components on page, use <Image /> Component instead of <img /> element & move avatar.webp to _assets folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,49 @@
+import Image from "next/image";
+import avatarImg from "./_assets/avatar.webp";
+
 export default function Home() {
   return (
     <div className="w-full grid grid-cols-1 gap-12 max-w-[400px]">
       <main>
-        <div>
-          <img src="./avatar.webp" alt="Jakub Jurkian" width="24px" />
-          <div>
-            <span>👋</span> Open to work
+        <div className="flex items-center gap-4 mb-6">
+          <Image
+            src={avatarImg}
+            alt="Jakub Jurkian"
+            placeholder="blur"
+            className="w-19 h-19 rounded-full object-cover border-2 border-border-color transition-all duration-200 hover:scale-105 hover:border-(--accent)"
+          />
+          <div className="bg-card px-4 py-2 rounded-[20px] border border-border-color text-sm font-medium text-(--text-secondary) w-[148px] text-center">
+            <span className="wave">👋</span> Open to work
           </div>
         </div>
-
-        <div>
-          <h1>I&apos;m Jakub Jurkian</h1>
-          <h2>
-            Web Developer specializing in <span>React</span> &<span>.NET</span>
+        <div className="text-center">
+          <h1 className="text-[clamp(2.5rem,5vw+1rem,4rem)] leading-[1.1] mb-3 font-bold tracking-[-0.02em]">
+            I&apos;m Jakub Jurkian
+          </h1>
+          <h2 className="text-lg text-(--text-secondary) font-normal mb-8">
+            Web Developer specializing in{" "}
+            <span className="text-(--text-primary) font-medium">React</span> &
+            <span className="text-(--text-primary) font-medium"> .NET</span>
           </h2>
 
-          <p>
+          <p className="text-(--text-secondary) text-base leading-[1.6] max-w-full mb-10">
             Building scalable Applications with clean architecture. Currently
             studying IT at the University of Gdańsk.
           </p>
 
-          <a href="resume.pdf" download>
+          <a
+            href="./resume.pdf"
+            className="block w-full text-center bg-(--accent) text-(--bg-main) px-8 py-[0.9rem] rounded-lg font-semibold text-base no-underline transition-all duration-200 hover:bg-[#fde047]"
+          >
             Download Resume
           </a>
         </div>
       </main>
 
-      <nav className="grid grid-cols-1 gap-4 bg-card">
+      <nav className="grid grid-cols-1 gap-4">
         <a href="projects.html" className="bento-box">
-          <span>
+          <span className="icon">
             <svg
-              width="24px"
               viewBox="0 0 1920 1920"
               xmlns="http://www.w3.org/2000/svg"
             >
@@ -42,9 +55,8 @@ export default function Home() {
         </a>
 
         <a href="about.html" className="bento-box">
-          <span>
+          <span className="icon">
             <svg
-              width="24px"
               viewBox="0 0 52 52"
               data-name="Layer 1"
               id="Layer_1"
@@ -64,8 +76,8 @@ export default function Home() {
           className="bento-box"
           target="_blank"
         >
-          <span>
-            <svg width="24px" viewBox="0 0 24 24">
+          <span className="icon">
+            <svg viewBox="0 0 24 24">
               <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
             </svg>
           </span>
@@ -78,9 +90,8 @@ export default function Home() {
           className="bento-box"
           target="_blank"
         >
-          <span>
+          <span className="icon">
             <svg
-              width="24px"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 50 50"
             >
@@ -92,8 +103,9 @@ export default function Home() {
         </a>
       </nav>
 
-      <footer>
-        <a href="mailto:kuba.jur03@gmail.com">Email</a>
+
+      <footer className="text-center text-(--text-secondary) mt-8 col-span-full">
+        <a href="mailto:kuba.jur03@gmail.com" className="text-(--text-secondary) transition-colors duration-200 no-underline  hover:text-(--accent)">Email</a>
       </footer>
     </div>
   );
